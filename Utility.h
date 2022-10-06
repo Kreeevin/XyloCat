28c5221e1138601db5bfb1de1b00d9e221421a5b
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2022-10-06 13:22:03 -0400

    Progress on serial to note conversion

--- Utility.h
+++ Utility.h
@@ -5,14 +5,24 @@
 #include <Arduino.h>
 
 typedef struct Note{
-  char noteName;
+  String noteName;
   int duration;
-  inline static int noteToPosition(char noteName){
+  inline static int noteToPosition(String noteName){
     return 0;
   };
-  inline static int noteToMotorSpeed(char noteName){
+  inline static int noteToMotorSpeed(String noteName){
     return 0;
   }
 };
+
+// function for converting MIDI info from serial buffer to Note
+// serial input is done in batches of two (with the lab example of inByte1 and inByte2) (?) 
+Note bufferToNote(byte[] buf){
+  //buf[0] is pitch, [1] is velocity
+  struct Note theNote;
+  theNote.noteName = ""; //figure this out later
+  theNote.duration = buf[1];
+  return theNote;
+}
 
 #endif
